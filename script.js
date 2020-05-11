d4be3f192f7183081db99f2b13db36b2779015ad
Johnny Minnesota <jminnesota1972@gmail.com>
2020-05-11 15:47:18 -0600

    reset and resize functions

--- script.js
+++ script.js
@@ -2,25 +2,15 @@ const curry = (fn) => (...a) => (...b) => fn(...a, ...b);
 const compose = (f, g) => (a) => (f(g(a)));
 const $ = (id) => document.getElementById(id);
 
-const newDiv = () => {
-    const div = document.createElement('div');
-    return div;
-};
-
-const addClass = (elem) => {
-    elem.classList.add('Box');
-    return elem;
-};
-
-const addEvent = curry((listener, elem) => {
-    elem.addEventListener('mouseover', listener);
-    console.log('tet');
+const addClass = curry((className, elem) => {
+    elem.classList.add(className);
     return elem;
 });
 
-const colorChange = (e) => e.target.classList.add('hit');
-
-const newBox = compose(addEvent(colorChange), compose(addClass, newDiv));
+const addEvent = curry((listener, elem) => {
+    elem.addEventListener('mouseover', listener);
+    return elem;
+});
 
 const appendChildren = (container, factory, num) => {
     if (num > 0) {
@@ -30,11 +20,24 @@ const appendChildren = (container, factory, num) => {
     return container;
 };
 
-const makeRow = (length) => appendChildren(newDiv(), newBox, length);
-const makeBoard = (size) => appendChildren($('container'), () => makeRow(size), size);
-
 const init = function() {
-    makeBoard(64);
+    const newDiv = () => document.createElement('div');
+    const colorChange = (e) => e.target.classList.add('hit');
+    const newBox = compose(addEvent(colorChange), compose(addClass('Box'), newDiv));
+
+    const makeRow = (size) => appendChildren(newDiv(), newBox, size);
+    const makeGrid = (size) => appendChildren($('container'), () => makeRow(size), size);
+
+    const getNewSize = () => prompt("Enter the size of the grid");
+    const resizeGrid = compose(makeGrid, getNewSize);
+
+    const colorReset = (div) => div.classList.remove('hit');
+    const resetGrid = () => [...document.querySelectorAll(".Box")].map(colorReset);
+    const cleargrid = () => $('container').innerHTML = '';
+
+    makeGrid(8);
+    $('resetButton').addEventListener('click', resetGrid);
+    $('resizeButton').addEventListener('click', compose(resizeGrid, cleargrid));
 }
 
 window.onload = init;
